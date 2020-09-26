f497cc59c8923cfc1504566b28c5ea6482a0195f
Silvacus <49201311+Silvacus@users.noreply.github.com>
2020-09-26 19:47:26 +0530

    Update SnakeAndLadder.java

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,6 +3,8 @@ public class SnakeAndLadder {
 	static final int START_POSITION=0;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder game");
+
+		int dice_value=1+(int) Math.floor(Math.random()*10)%6;
 		
 	}
 }
